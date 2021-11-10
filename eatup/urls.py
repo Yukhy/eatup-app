20fffc56922561d439f370eb918a7288783f8bd7
Yukihiro Kozai <50065338+Yukhy@users.noreply.github.com>
2021-11-10 14:00:11 +0900

    Initial commit

---
+++ eatup/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from . import views
+
+app_name = 'eatup'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('shopping-cart/', views.shopping_cart, name='shopping-cart'),
+    path('notification/', views.notification, name='notification'),
+    path('user-info/', views.user_info, name='user-info'),
+]
---
+++ eatup/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'eatup/index.html')
+
+def shopping_cart(request):
+    return render(request, 'eatup/shoppingcart.html')
+
+def notification(request):
+    return render(request, 'eatup/notification.html')
+
+def user_info(request):
+    return render(request, 'eatup/usermenu.html')
